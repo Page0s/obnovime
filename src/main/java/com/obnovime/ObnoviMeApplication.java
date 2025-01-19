729ccad6e5840cf6201c720917ad593cb3f9a75a
Bojan Pađen <game4rge@gmail.com>
2025-01-19 15:27:27 +0100

    first commit

---
+++ src/main/java/com/obnovime/ObnoviMeApplication.java
@@ -0,0 +1,13 @@
+package com.obnovime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObnoviMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObnoviMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obnovime/ObnoviMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obnovime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObnoviMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
