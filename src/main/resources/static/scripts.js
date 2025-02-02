ca4858a981f412db4d00c9e7d3b48998e776c6a4
Bojan Pađen <game4rge@gmail.com>
2025-02-02 21:33:02 +0100

    Update Main, Edit and Renewal for toast msg and pop-up

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,7 +1,7 @@
 function showForm() {
     var selectedValue = document.getElementById("documentType").value;
     var forms = document.querySelectorAll(".form-container");
-    var backButton = document.querySelector(".container > a.btn-light");
+    var backButton = document.querySelector(".container > a.btn-secondary");
 
     // Sakrij sve forme
     forms.forEach(function (form) {
@@ -32,6 +32,15 @@ function showForm() {
                 }
             });
         });
+        function openConfirmationModalRenewal() {
+            const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModalRenewal'));
+            confirmationModal.show();
+        }
+
+        function submitForm() {
+            sessionStorage.setItem('showToast', 'true');
+            document.getElementById('renewalForm').submit();
+        }
     function openConfirmationModal(event) {
         event.preventDefault();
         formToSubmit = event.target;
@@ -48,3 +57,28 @@ document.addEventListener("DOMContentLoaded", function() {
         sessionStorage.removeItem("showToast"); // Briše toast nakon prikazivanja
     }
 });
+ function validateReminder() {
+            let reminderField = document.getElementById("renewalPeriod");
+            let errorMsg = document.getElementById("error-message");
+
+            if (isNaN(reminderField.value) || reminderField.value.trim() === "") {
+                errorMsg.style.display = "block";
+                return false;
+            } else {
+                errorMsg.style.display = "none";
+                return true;
+            }
+        }
+
+        function showConfirmationModal(event) {
+            event.preventDefault(); // Spriječi automatsko slanje forme
+            if (validateReminder()) {
+                let modal = new bootstrap.Modal(document.getElementById('confirmationModalEdit'));
+                modal.show();
+            }
+        }
+
+        function submitFormEdit() {
+            sessionStorage.setItem('showToast', 'true');
+            document.getElementById('editForm').submit();
+        }
