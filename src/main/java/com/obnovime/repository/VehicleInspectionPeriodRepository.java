b14afb7142f29cf7fd2fadf3d02dd0d07a2489cc
Bojan Pađen <page0itv@gmail.com>
2025-02-14 18:58:28 +0100

    update /dokument/{id}/obnova

--- src/main/java/com/obnovime/repository/VehicleInspectionPeriodRepository.java
+++ src/main/java/com/obnovime/repository/VehicleInspectionPeriodRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 @Repository
 public interface VehicleInspectionPeriodRepository extends JpaRepository<VehicleInspectionPeriod, Long> {
     Optional<VehicleInspectionPeriod> findByDescription(String description);
-    Optional<VehicleInspectionPeriod> findById(Long id);
 }
